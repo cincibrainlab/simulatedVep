5bd68b12352d7ee80e98275f9586c8cd98f9f472
Ernie Pedapati <ernest.pedapati@cchmc.org>
2022-12-08 16:51:11 -0500

    added dcs

--- eeg_htpEegSimulateVepTest.m
+++ eeg_htpEegSimulateVepTest.m
@@ -1,24 +1,3 @@
-# Simulated Visual Evoked Potential
-This is a function that generates simulated electroencephalography (EEG) data for a visual evoked potential (VEP) experiment. It takes various parameters as inputs and returns a structure containing the simulated EEG data, the average VEP waveform, the time values corresponding to each sample in the EEG data, and information about the channels in the EEG data. It can be used to test algorithms that analyze VEP data or to train machine learning models. The function uses the input arguments to control the characteristics of the generated data, such as the latencies and amplitudes of the VEP components, the number of trials and channels, and the sample rate and trial length.
-![VEP trace](vepmean.png)
-
-Getting Started
-To use this code, download or clone the repository and run the simulated_VEP.m file in Matlab.
-EEGLAB is a prerequisite to build the EEG SET file
-
-Prerequisites
-This code requires Matlab to run.
-
-Focus of VEP is "Oz" equivelent on EGI 128 net (with a gaussian distribution of the peak)
-![VEP topographic distribution](vepfocus.png)
-
-Output is a standard EEG SET file
-![VEP channel trace](veptrace.png)
-
-Usage:
-see eeg_htpEegStimulateVepTest.m
-
-```
 % generate simulated EEG data with default arguments
 EEG = eeg_htpEegVepGenerator();
 
@@ -75,15 +54,3 @@ EEG = eeg_htpEegVepGenerator(...
     'noiseAmplitude', noiseAmplitude, ...
     'jitterRange', jitterRange, ...
     'peak_channel', peak_channel);
-```
-
-
-Built With
-Matlab - The programming language used
-Authors
-E. Pedapati - Initial work - GitHub User
-See also the list of contributors who participated in this project.
-
-License
-This project is licensed under the MIT License - see the LICENSE.md file for details
-
